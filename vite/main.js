feb446f915df02af7a569736534a07a030508070
LeaderLou1 <louis28jager@gmail.com>
2024-01-23 15:15:27 -0500

    dish

--- vite/main.js
+++ vite/main.js
@@ -8,7 +8,9 @@ const testRoute = async (url) => {
 
   const url1 = 'https://www.themealdb.com/api/json/v1/1/list.php?a=list';
   const url2 = 'https://www.themealdb.com/api/json/v1/1/filter.php?a=Jamaican';
-  const url3 = 'https://www.themealdb.com/api/json/v1/1/search.php?s=Jamaican%20Beef%20Patties'
+
+  const dishId = 52936
+  const url3 = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${dishId} `
  
   testRoute(url1);
   testRoute(url2);
