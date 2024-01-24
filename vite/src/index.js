7d4b093c335d59651c3909a660842bd672f47484
LeaderLou1 <louis28jager@gmail.com>
2024-01-24 15:37:59 -0500

    update

--- vite/src/index.js
+++ vite/src/index.js
@@ -32,11 +32,12 @@ const getFoodData = async () => {
 
             const li = document.createElement('li');
 
+            //Implemented Event Listener for Button
             button.addEventListener('click', async (e) => {
                 if(food.contains(e.target)){
                 const mealId = e.target.dataset.id
                 console.log(`Meal ID: ${mealId}`);
-                
+                //Fetch meal id
                 const foodId = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`
 
                     try{
@@ -47,6 +48,10 @@ const getFoodData = async () => {
                         }
                         const fullRecipe = await response.json();
                         console.log(`Full Recipe:`, fullRecipe);
+
+                        const { strInstructions} = fullRecipe.meals[0];
+                        console.log(`strInstructions: ${strInstructions}`);
+                        
 
                     }
                     catch(error){
